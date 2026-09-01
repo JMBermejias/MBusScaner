5893c6b8bf37c045d805f7cb0c344692ce3dbf79
Jose Manuel Bernabeu Mejias <jmbernab@users.noreply.github.com>
2026-09-01 16:59:10 +0200

    Versión 1.0.1: ventana maximizada y alta manual de dispositivos

--- MBusScaner/ViewModels/SettingsViewModel.cs
+++ MBusScaner/ViewModels/SettingsViewModel.cs
@@ -32,6 +32,9 @@ namespace MBusScaner.ViewModels
             _pollInterval = _settings.PollingIntervalMs.ToString();
         }
 
+        public string AppVersion =>
+            System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "1.0.0";
+
         public string LocalIP
         {
             get => _localIP;
