aea020974598a4e0106a07a6050f6c59481bdaf0
Jose Manuel Bernabeu Mejias <jmbernab@users.noreply.github.com>
2026-09-04 16:29:48 +0200

    v1.0.8: Force maximized window on open (Wayland fix)

--- MBusScaner.Avalonia/Views/MainView.axaml.cs
+++ MBusScaner.Avalonia/Views/MainView.axaml.cs
@@ -1,13 +1,19 @@
 using Avalonia.Controls;
+using Avalonia.Threading;
 
-using MBusScaner.Avalonia.ViewModels; namespace MBusScaner.Avalonia.Views
+using MBusScaner.Avalonia.ViewModels;
+namespace MBusScaner.Avalonia.Views
 {
     public partial class MainView : Window
     {
         public MainView()
         {
             InitializeComponent();
+            Opened += (_, _) =>
+            {
+                if (WindowState != WindowState.Maximized)
+                    WindowState = WindowState.Maximized;
+            };
         }
     }
 }
-
